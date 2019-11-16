acf0249bdbfc4d396e0e0e7fbe4dead06bcd90b8
Marat Nasibyan ashoti <marat_17.5@live.com>
2019-11-16 20:20:22 +0400

    initial commit

---
+++ Trainings/Trainings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Trainings
+{
+    class Program
+    {
+        /*class exception*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
