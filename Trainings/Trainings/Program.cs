eb56602873aeb8a889d2de60f602a552e5e1a691
Marat Nasibyan ashoti <marat_17.5@live.com>
2019-11-16 21:06:22 +0400

    Add Exception1, Exception2, Exception3, Exception4 methods.

--- Trainings/Trainings/Program.cs
+++ Trainings/Trainings/Program.cs
@@ -4,10 +4,95 @@ namespace Trainings
 {
     class Program
     {
-        /*class exception*/
+        #region Exception
+        public static void Exception1()
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"result: {y}");                   
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"it is exception {ex.Message}");
+            }            
+        }
+
+        public static void Exception2()
+        {
+            int x = 1;
+            int y = 0;
+
+            try
+            {
+                int result = x / y;
+            }
+            catch (DivideByZeroException) when (y == 0 && x == 0)
+            {
+                Console.WriteLine("y не должен быть равен 0");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void Exception3()
+        {
+            // in this case when there is first excepton 2nd exception doesn't call
+            try
+            {
+                int[] numbers = new int[4];
+                numbers[7] = 9;     // IndexOutOfRangeException
+
+                int x = 5;
+                int y = x / 0;  // DivideByZeroException
+                Console.WriteLine($"result: {y}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("it is exception DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+
+        public static void Exception4()
+        {
+            // in this case when there is many exception
+            // exception class catchs all exception except DivideByZeroException and IndexOutOfRangeException
+            try
+            {
+                object obj = "you";
+                int num = (int)obj;     // InvalidCastException
+                Console.WriteLine($"Результат: {num}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Возникло исключение IndexOutOfRangeException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+            Console.Read();
+        }
+
+        #endregion
 
         static void Main(string[] args)
         {
+            //Exception1();
+            Exception2();
             Console.WriteLine("Hello World!");
         }
     }
