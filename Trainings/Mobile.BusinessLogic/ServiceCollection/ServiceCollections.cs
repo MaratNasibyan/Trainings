630abdefa65f4c3ddf2865c43d255f474d89d38e
Marat Nasibyan ashoti <marat_17.5@live.com>
2020-01-12 17:29:21 +0400

    Add other tables to generate sql script

--- Trainings/Mobile.BusinessLogic/ServiceCollection/ServiceCollections.cs
+++ Trainings/Mobile.BusinessLogic/ServiceCollection/ServiceCollections.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Mobile.Abstraction;
 using Mobile.Repository.RepositoryCollection;
--- Trainings/Mobile.DBMap/Scripts/GenerateFund.sql
+++ Trainings/Mobile.DBMap/Scripts/GenerateFund.sql
@@ -8,11 +8,65 @@ CREATE TABLE dbo.Fundraiser
 	[LastName] NVARCHAR(MAX) NULL,
 	[Email] NVARCHAR(MAX) NOT NULL,
 	[Description] NVARCHAR(MAX) NULL,
-	[CreateDate] DATETIME ,
+	[CreateDate] DATETIME NOT NULL,
 	[ModifyDate] DATETIME NULL,
 	[IsActive] BIT,
 
-	CONSTRAINT PK_Fundraiser PRIMARY KEY ([FundraiserId])
+	CONSTRAINT PK_Fundraiser_FundraiserId PRIMARY KEY ([FundraiserId])
 )
 
 GO
+
+CREATE TABLE dbo.Donate
+(
+	[DonateId] BIGINT NOT NULL IDENTITY(1, 1),
+	[Money] MONEY NOT NULL,
+	[FundraiserId] BIGINT NOT NULL,
+	[CreateDate] DATETIME NOT NULL,
+	[ModifyDate] DATETIME NULL,
+	[IsActive] BIT
+
+	CONSTRAINT PK_Donate_DonateId PRIMARY KEY ([DonateId])
+	CONSTRAINT FK_Fundraiser_Donate FOREIGN KEY ([FundraiserId]) REFERENCES Fundraiser([FundraiserId])
+)
+
+GO
+
+CREATE TABLE dbo.People
+(
+	[PeopleId] BIGINT NOT NULL IDENTITY(1, 1),
+	[CreateDate] DATETIME NOT NULL,
+	[ModifyDate] DATETIME NULL,
+	[IsActive] BIT
+
+	CONSTRAINT PK_People_PeopleId PRIMARY KEY ([PeopleId])
+)
+
+GO	
+
+CREATE TABLE dbo.FundraiserPeople
+(
+	[FundraiserPeopleId] BIGINT NOT NULL IDENTITY(1, 1),
+	[FundraiserId] BIGINT NOT NULL,
+	[PeopleId] BIGINT NOT NULL,
+	[CreateDate] DATETIME NOT NULL,
+	[ModifyDate] DATETIME NULL,
+	[IsActive] BIT
+
+	CONSTRAINT PK_FundraiserPeople_FundraiserPeopleId PRIMARY KEY ([FundraiserPeopleId]),
+	CONSTRAINT FK_Fundraiser_People FOREIGN KEY ([FundraiserId]) REFERENCES Fundraiser(FundraiserId),
+	CONSTRAINT FK_People_Fundraiser FOREIGN KEY ([PeopleId]) REFERENCES People([PeopleId])
+)
+
+GO
+
+CREATE TABLE dbo.Pictures
+(
+	[PictureId] BIGINT NOT NULL IDENTITY(1, 1),
+	[PicturePath] NVARCHAR(MAX) NOT NULL,
+	[CreateDate] DATETIME NOT NULL,
+	[ModifyDate] DATETIME NULL,
+	[IsActive] BIT
+
+	CONSTRAINT PK_Pictures_PictureId PRIMARY KEY ([PictureId])
+)
